c5de31bb88346d675d59a8ceafcf3ca3ae28fc5e
Pablo Lopez <pablo.lopez@ndeveloper.com>
2016-08-01 10:48:05 -0500

    Primera carga del proyecto

---
+++ bdd/carga_ejempl.sql
@@ -0,0 +1,36 @@
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',1,'Freedom','Rojo',0.5,10,0.25,'USA',0.26);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',2,'Freedom','Rojo',0.5,20,0.3,'Rusia',0.3);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',3,'Freedom','Rojo',0.5,30,0.25,'USA',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',4,'Freedom','Rojo',0.5,40,0.45,'Nueva Zelanda',0.45);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',5,'Freedom','Rojo',0.5,50,0.25,'Belgica',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',6,'Freedom','Rojo',0.5,60,0.25,'USA',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',7,'Freedom','Rojo',0.5,70,0.25,'USA',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',8,'Freedom','Rojo',0.5,80,0.4,'USA',0.4);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 1',9,'Freedom','Rojo',0.5,90,0.25,'USA',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',1,'Freedom','Rojo',0.5,10,0.27,'USA',0.26);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',2,'Freedom','Rojo',0.5,20,0.3,'USA',0.3);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',3,'Freedom','Rojo',0.5,30,0.25,'USA',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',4,'Freedom','Rojo',0.5,40,0.45,'USA',0.45);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',5,'Freedom','Rojo',0.5,50,0.25,'Nueva Zelanda',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',6,'Freedom','Rojo',0.5,60,0.25,'Belgica',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',7,'Freedom','Rojo',0.5,70,0.25,'USA',0.25);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',8,'Freedom','Rojo',0.5,80,0.4,'USA',0.4);
+insert into ejemplo_ventas (empresa,mes,variedad,color,largo,tallos,precio,destino,promedios) values (
+'Floricola 2',9,'Freedom','Rojo',0.5,90,0.25,'USA',0.25);
